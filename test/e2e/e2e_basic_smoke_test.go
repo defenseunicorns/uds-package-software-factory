cdf1a5b551f199364dfcd3652bea5a528f9bd9cb
Michael Kruggel <108417058+Michael-Kruggel@users.noreply.github.com>
2023-07-25 14:38:31 -0700

    Add gitlab-runner to swf (#39)
    Co-authored-by: Anthony <anthony@defenseunicorns.com>

--- test/e2e/e2e_basic_smoke_test.go
+++ test/e2e/e2e_basic_smoke_test.go
@@ -28,11 +28,11 @@ func TestAllServicesRunning(t *testing.T) { //nolint:funlen
 		require.NoError(t, err, output)
 
 		// Wait for the GitLab Webservice Deployment to exist.
-		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c "while ! kubectl get deployment gitlab-gitlab-webservice-default -n gitlab; do sleep 5; done"`)
+		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c "while ! kubectl get deployment gitlab-webservice-default -n gitlab; do sleep 5; done"`)
 		require.NoError(t, err, output)
 
 		// Wait for the GitLab Webservice Deployment to report that it is ready
-		output, err = platform.RunSSHCommandAsSudo(`kubectl rollout status deployment/gitlab-gitlab-webservice-default -n gitlab --watch --timeout=1200s`)
+		output, err = platform.RunSSHCommandAsSudo(`kubectl rollout status deployment/gitlab-webservice-default -n gitlab --watch --timeout=1200s`)
 		require.NoError(t, err, output)
 
 		// Ensure that the services do not accept discontinued TLS versions. If they reject TLSv1.1 it is assumed that they also reject anything below TLSv1.1.
@@ -47,5 +47,14 @@ func TestAllServicesRunning(t *testing.T) { //nolint:funlen
 		// Ensure that GitLab is available outside of the cluster.
 		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c "while ! curl -L -s --fail --show-error https://gitlab.bigbang.dev/-/health > /dev/null; do sleep 5; done"`)
 		require.NoError(t, err, output)
+
+		// Wait for the GitLab Runner Deployment to exist.
+		output, err = platform.RunSSHCommandAsSudo(`timeout 1200 bash -c "while ! kubectl get deployment gitlab-runner -n gitlab-runner; do sleep 5; done"`)
+		require.NoError(t, err, output)
+
+		// Wait for the GitLab Runner Deployment to report that it is ready
+		output, err = platform.RunSSHCommandAsSudo(`kubectl rollout status deployment/gitlab-runner -n gitlab-runner --watch --timeout=1200s`)
+		require.NoError(t, err, output)
+
 	})
 }
