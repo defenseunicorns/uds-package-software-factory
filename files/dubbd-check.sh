efb5bd341032efb97850d00849247b0c26209262
Michael Kruggel <michael.kruggel@defenseunicorns.com>
2023-07-28 13:31:25 -0700

    Shellchecked

--- files/dubbd-check.sh
+++ files/dubbd-check.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-MINIMUM_VERSION=""
+MINIMUM_VERSION="0.5.0"
 
 for (( i=0; i<${#MINIMUM_VERSION}; i++ )); do
-  LENGTH=$(printf "$LENGTH━")
+  LENGTH=$(printf "%s━" "$LENGTH")
 done
 
 DEPLOYED_VERSION=$(kubectl get secret -n zarf --no-headers=true | awk '/dubbd/{print $1}' | xargs kubectl get secret -n zarf -o=jsonpath='{.data.data}' | base64 -d | jq -r .data.metadata.version)
@@ -12,9 +12,9 @@ DEPLOYED_VERSION=$(kubectl get secret -n zarf --no-headers=true | awk '/dubbd/{p
 OLDER_VERSION=$(echo -e "${DEPLOYED_VERSION}\n${MINIMUM_VERSION}" | sort -V | head -n1)
 
 if [[ "${OLDER_VERSION}" != "${MINIMUM_VERSION}" ]]; then
-  printf "\033[1;91m┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━$LENGTH┓\033[0m\n"
-  printf "\033[1;91m┃ dubbd is older than minimum version: $MINIMUM_VERSION ┃\033[0m\n"
-  printf "\033[1;91m┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━$LENGTH┛\033[0m\n"
+  printf "\033[1;91m┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━%s┓\033[0m\n" "$LENGTH"
+  printf "\033[1;91m┃ dubbd is older than minimum version: %s ┃\033[0m\n" "$MINIMUM_VERSION"
+  printf "\033[1;91m┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━%s┛\033[0m\n" "$LENGTH"
   echo
   exit 1
 else
